cd2f36950bf6302038db3c07806d1bff46674a37
Floof <98069058+FloofCat@users.noreply.github.com>
2023-09-12 00:45:54 +0530

    Add files via upload

--- Keyboard? Login?/server.js
+++ Keyboard? Login?/server.js
@@ -2,13 +2,22 @@ const express = require('express')
 const path = require('path');
 const app = express();
 
-const port = 5005
+const port = 5000
 
-app.use('/chall2/',express.static('public'))
+app.use(express.static('public'))
 
-app.get("/chall2/",(req,res)=> {
+app.get("/",(req,res)=> {
     res.status(200).sendFile(path.join(__dirname, '/index.html'));
 })
+
+app.get("/login.js",(req,res)=> {
+    res.status(200).sendFile(path.join(__dirname, '/login.js'));
+})
+
+app.get("/flag-bits-floof.html",(req,res)=> {
+    res.status(200).sendFile(path.join(__dirname, '/flag-bits-floof.html'));
+})
+
 
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
