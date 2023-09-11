4ab310c5dd04670a688ab04531e130865a806890
Kalash Poddar <kalashpoddar33@gmail.com>
2023-09-12 01:59:27 +0530

    updated

--- Keyboard? Login?/server.js
+++ Keyboard? Login?/server.js
@@ -3,10 +3,10 @@ const bodyParser = require('body-parser');
 const fs = require('fs');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5005;
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(__dirname + '/public'));
+app.use('/chall2', express.static(__dirname + '/public'));
 
 // Simulated database of valid users
 const users = [
@@ -17,14 +17,14 @@ app.get('/chall2/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-app.post('/login', (req, res) => {
+app.post('/chall2/login', (req, res) => {
   const { username, password } = req.body;
 
   // Check if the provided credentials match a valid user
   const validUser = users.find(user => user.username === username && user.password === password);
 
   if (validUser) {
-    res.redirect('/flag-bits-floof.html');
+    res.redirect('/chall2/flag-bits-floof.html');
   } else {
     res.send('Invalid login credentials. Please try again.');
   }
