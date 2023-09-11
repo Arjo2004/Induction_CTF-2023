94ddbde0c204f92edc60777f35630c7c02feb684
Floof <98069058+FloofCat@users.noreply.github.com>
2023-09-12 01:28:28 +0530

    Add files via upload

--- Keyboard? Login?/server.js
+++ Keyboard? Login?/server.js
@@ -1,23 +1,35 @@
-const express = require('express')
-const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
 const app = express();
+const port = process.env.PORT || 3000;
 
-const port = 5000
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/public'));
 
-app.use(express.static('public'))
+// Simulated database of valid users
+const users = [
+  { username: 'floofcat', password: 'bitskrieg@69420' },
+];
 
-app.get("/",(req,res)=> {
-    res.status(200).sendFile(path.join(__dirname, '/index.html'));
-})
+app.get('/chall2/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
 
-app.get("/login.js",(req,res)=> {
-    res.status(200).sendFile(path.join(__dirname, '/login.js'));
-})
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
 
-app.get("/flag-bits-floof.html",(req,res)=> {
-    res.status(200).sendFile(path.join(__dirname, '/flag-bits-floof.html'));
-})
+  // Check if the provided credentials match a valid user
+  const validUser = users.find(user => user.username === username && user.password === password);
 
+  if (validUser) {
+    res.redirect('/flag-bits-floof.html');
+  } else {
+    res.send('Invalid login credentials. Please try again.');
+  }
+});
 
-app.listen(port);
-console.log('Server started at http://localhost:' + port);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
